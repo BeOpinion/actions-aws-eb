79033fc4dcaba7bf351327ec0b8926205c75812f
Jean BOUDET <jeanboudet@officejean.home>
2023-02-07 11:16:22 +0100

    Fix Github trouble with repo ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
+# Add safe directory option for github workspace to disable fatal error
+#  - docker container uid is 0(root)
+#  - github workspace directory owner is 1001
+git config --global --add safe.directory /github/workspace
+
 eval $1
